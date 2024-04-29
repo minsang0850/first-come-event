e3b3475cf63c45c9eb5c546c91e678d85b9354cb
mingsang <minsang0850@naver.com>
2024-04-29 23:17:09 +0900

    init

---
+++ src/main/java/minsang/firstcomeevent/FirstComeEventApplication.java
@@ -0,0 +1,13 @@
+package minsang.firstcomeevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstComeEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstComeEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minsang/firstcomeevent/FirstComeEventApplicationTests.java
@@ -0,0 +1,13 @@
+package minsang.firstcomeevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstComeEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
